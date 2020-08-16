535fb1ae0e7dcf28e1bf831a7800664e8e2aa034
Keith Cirkel <keithamus@users.noreply.github.com>
2020-08-17 00:00:49 +0100

    fix: export everything to mod.ts

--- mod.ts
+++ mod.ts
@@ -1,2 +1,12 @@
-export { Scanner } from "./scanner.ts";
+export {
+  Scanner,
+  Token,
+  scanWhitespace,
+  scanIdents,
+  scanInts,
+  scanFloats,
+  scanStrings,
+  scanComments,
+  defaultTokens,
+} from "./scanner.ts";
 export { TokenError, nextTokenIs } from "./helpers.ts";
